2e2cff22ceebf22d35a06c7442930576da2353f1
Rodrigo Fernández <rodrigo@aquih.com>
2022-08-03 15:43:37 +0000

    Formatos para cheques

--- report/__init__.py
+++ report/__init__.py
@@ -1,3 +1,3 @@
 # -*- encoding: utf-8 -*-
 
-from . import report_invoice
+from . import report_invoice, report_payment
---
+++ report/report_payment.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+from odoo import api, models
+from odoo.addons.l10n_gt_extra import a_letras
+
+class ReportAbstractPayment(models.AbstractModel):
+    _name = 'rolsa.abstract.reporte_account_payment'
+    
+    def totales(self, o):
+        t = {'debito': 0, 'credito': 0}
+        for l in o.move_id.line_ids:
+            t['debito'] += l.debit
+            t['credito'] += l.credit
+        return t
+    
+    def a_letras(self,monto):
+        return a_letras.num_a_letras(monto)
+    
+    def _get_report_values(self, docids, data=None):
+        model = 'account.payment'
+        docs = self.env['account.payment'].browse(docids)
+        
+        return {
+            'doc_ids': docids,
+            'doc_model': model,
+            'docs': docs,
+            'data': data,
+            'a_letras': self.a_letras,
+            'totales': self.totales,
+        }
+        
+class ReportPayment1(models.AbstractModel):
+    _name = 'report.rolsa.reporte_account_payment1'
+    _inherit = 'rolsa.abstract.reporte_account_payment'
+    
+class ReportPayment2(models.AbstractModel):
+    _name = 'report.rolsa.reporte_account_payment2'
+    _inherit = 'rolsa.abstract.reporte_account_payment'
+
+class ReportPayment3(models.AbstractModel):
+    _name = 'report.rolsa.reporte_account_payment3'
+    _inherit = 'rolsa.abstract.reporte_account_payment'
+
+class ReportPayment4(models.AbstractModel):
+    _name = 'report.rolsa.reporte_account_payment4'
+    _inherit = 'rolsa.abstract.reporte_account_payment'
+    
+class ReportPayment5(models.AbstractModel):
+    _name = 'report.rolsa.reporte_account_payment5'
+    _inherit = 'rolsa.abstract.reporte_account_payment'
+    
+class ReportPayment6(models.AbstractModel):
+    _name = 'report.rolsa.reporte_account_payment6'
+    _inherit = 'rolsa.abstract.reporte_account_payment'
+    
+class ReportPayment7(models.AbstractModel):
+    _name = 'report.rolsa.reporte_account_payment7'
+    _inherit = 'rolsa.abstract.reporte_account_payment'
+    
+class ReportPayment8(models.AbstractModel):
+    _name = 'report.rolsa.reporte_account_payment8'
+    _inherit = 'rolsa.abstract.reporte_account_payment'
+    
+class ReportPayment9(models.AbstractModel):
+    _name = 'report.rolsa.reporte_account_payment9'
+    _inherit = 'rolsa.abstract.reporte_account_payment'
